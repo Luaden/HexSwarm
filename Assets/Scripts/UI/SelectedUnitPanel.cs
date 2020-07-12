eb2789f1a0f174ea95451de2e2c9c6b4bd050d97
Ace Young <luadengames@gmail.com>
2020-07-12 17:30:32 +0200

    Updated Selected Unit Panel
    Accounts for abilities, icon, setting background color when selected.

--- Assets/Scripts/UI/SelectedUnitPanel.cs
+++ Assets/Scripts/UI/SelectedUnitPanel.cs
@@ -10,30 +10,40 @@ public class SelectedUnitPanel : CoreUIElement<IUnit>
     [SerializeField] protected TMP_Text unitDescription;
 
     [SerializeField] protected Image ability1Image;
+    [SerializeField] protected Image ability1Background;
+    [SerializeField] protected Button ability1Button;
     [SerializeField] protected TMP_Text ability1Name;
     [SerializeField] protected TMP_Text ability1Description;
 
-    [SerializeField] protected Image ability2Image;   
+    [SerializeField] protected Image ability2Image;
+    [SerializeField] protected Image ability2Background;
+    [SerializeField] protected Button ability2Button;
     [SerializeField] protected TMP_Text ability2Name;    
     [SerializeField] protected TMP_Text ability2Description;
+
+    protected GameManager gameManager;
+    protected Color playerColor;
+
+    protected bool firstIsSelected;
+    public bool FirstIsSelected => firstIsSelected;
 
     public override void UpdateUI(IUnit unit)
     {
         if (ClearedIfEmpty(unit))
             return;
-        //Need to also add a panel reference to team, OR color unit image to be team color.
+
         UpdateSprite(unitImage, unit.Icon);
         UpdateText(unitName, unit.Name);
         UpdateText(unitDescription, unit.Description);
 
         //This needs to be a single sprite representing the action, or we need a way to determine if it's damage or movement.
-        //UpdateSprite(ability1Image, unit.Abilites[0].DamageGrid);
-        //UpdateText(ability1Name, unit.Abilites[0].Name);
-        //UpdateText(ability1Description, unit.Abilites[0].Description);
+        UpdateSprite(ability1Image, unit.Abilites[0].DamageGrid);
+        UpdateText(ability1Name, unit.Abilites[0].Name);
+        UpdateText(ability1Description, unit.Abilites[0].Description);
 
-        //UpdateSprite(ability2Image, unit.Abilites[1].DamageGrid);
-        //UpdateText(ability2Name, unit.Abilites[1].Name);
-        //UpdateText(ability2Description, unit.Abilites[1].Description);
+        UpdateSprite(ability2Image, unit.Abilites[1].DamageGrid);
+        UpdateText(ability2Name, unit.Abilites[1].Name);
+        UpdateText(ability2Description, unit.Abilites[1].Description);
     }
 
     protected override bool ClearedIfEmpty(IUnit unit)
@@ -57,5 +67,18 @@ public class SelectedUnitPanel : CoreUIElement<IUnit>
         }
 
         return false;
+    }
+
+    public void SelectAbility(bool ability)
+    {
+        firstIsSelected = ability;
+
+        if(firstIsSelected)
+        {
+            ability1Background.color = Color.green;
+        }
+
+        ability1Background.color = firstIsSelected ? Color.white : Color.grey;
+        ability2Background.color = firstIsSelected ? Color.grey : Color.white;
     }
 }
