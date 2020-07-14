0b1e9d3742a10bf102de564a1d65dfa9850393c5
Ace Young <luadengames@gmail.com>
2020-07-14 17:05:12 +0200

    Updated Interfaces
    Changed IControlsConfig, added BattlefieldManager.

--- Assets/Scripts/Interfaces/ITeam.cs
+++ Assets/Scripts/Interfaces/ITeam.cs
@@ -2,28 +2,31 @@
 using UnityEngine.Tilemaps;
 using System.Collections.Generic;
 
-public interface ITeam
+namespace Old
 {
-    string Name { get; }
-    string Description { get; }
-    Sprite Icon { get; }
-    TileBase Tile { get; }
-    Color Color { get; }
-    Teams Member { get; }
-    bool HasMove { get; }
-    IGameManager GameManager { get; }
-    IEnumerable<IUnit> Units { get;}
-    IEnumerable<Vector3Int> HighlightMove { get; }
-    IEnumerable<Vector3Int> HighlightAttack { get; }
-    IEnumerable<Vector3Int> HighlightOverlap { get; }
+    public interface ITeam
+    {
+        string Name { get; }
+        string Description { get; }
+        Sprite Icon { get; }
+        TileBase Tile { get; }
+        Color Color { get; }
+        Teams Member { get; }
+        bool HasMove { get; }
+        IGameManager GameManager { get; }
+        IEnumerable<IUnit> Units { get; }
+        IEnumerable<Vector3Int> HighlightMove { get; }
+        IEnumerable<Vector3Int> HighlightAttack { get; }
+        IEnumerable<Vector3Int> HighlightOverlap { get; }
 
-    void GetUnit(IUnit unit);
-    //void Update();
-    bool HasUnitsAfterLosses(IEnumerable<IUnit> units);
-    void StartTurn();
-    bool Undo();
-    void EndTurn();
-    void DoMove(IUnit unit, IPosAbilityDefault ablity, Vector3Int target);
-    void ResolveHighlight(IUnit unit, IPosAbilityDefault ablity, Vector3Int target);
+        void GetUnit(IUnit unit);
+        //void Update();
+        bool HasUnitsAfterLosses(IEnumerable<IUnit> units);
+        void StartTurn();
+        bool Undo();
+        void EndTurn();
+        void DoMove(IUnit unit, IPosAbilityDefault ablity, Vector3Int target);
+        void ResolveHighlight(IUnit unit, IPosAbilityDefault ablity, Vector3Int target);
+    }
 }
 
