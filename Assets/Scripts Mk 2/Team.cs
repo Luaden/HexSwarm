08c46cf23bcaf4f3592aed31ad4e6da0e3455118
Ace Young <luadengames@gmail.com>
2020-07-16 16:50:22 +0200

    ITeam, Team, AITeam all worked on
    This commit/push was a mistake.

---
+++ Assets/Scripts Mk 2/Team.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Team : ITeam
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public Sprite Icon { get; set; }
+    public Color Color { get; set; }
+    public Teams TeamNumber { get; set; }
+    public bool MyTurn { get; set; }
+    public IGameManager GameManager { get; set; }
+    public IEnumerable<IUnit> Units { get => units; }
+
+    protected List<IUnit> units;
+
+    public void StartTurn() => MyTurn = true;
+
+    public void EndTurn()
+    {
+        //Tell the game manager this team is done.
+    }
+
+    public void KillUnit(IUnit unit)
+    {
+        try
+        {
+            units.Remove(unit);
+        }
+        catch
+        {
+            Debug.LogError("Team " + Name + " was asked to remove a unit it did not own in unit list.");
+        }
+
+        if(units.Count <= 0)
+            TeamEliminated();
+    }
+
+    protected void TeamEliminated()
+    {
+        //Tell game manager this team is eliminated.
+    }
+
+}
