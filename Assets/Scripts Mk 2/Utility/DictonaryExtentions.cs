9ba6031f65f5b2d3ca913bdfa4f01e5437987ab0
Brian <whynotandwhy@gmail.com>
2020-07-24 20:35:39 -0400

    Extension tools/demos
    two new functionc one lets you add an an object to a Dictionary<X,Hashset<Y>>
    and one that lets you add the same tile repeatedly, need to examine later for performance profiling.

---
+++ Assets/Scripts Mk 2/Utility/DictonaryExtentions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictonaryExtentions
+{ 
+    public static class DictonaryExtentions
+    {
+        public static void JustAdd<TDictionary, TKey, TValue>(this TDictionary dic, TKey key, TValue value) where TDictionary : Dictionary<TKey, HashSet<TValue>>
+        {
+            HashSet<TValue> grouping;
+            if (!dic.TryGetValue(key, out grouping))
+            {
+                grouping = new HashSet<TValue>() { value };
+                dic.Add(key, grouping);
+            }
+            grouping.Add(value);
+        }
+    }
+}
---
+++ Assets/Scripts Mk 2/Utility/MapExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+namespace MapExtensions
+{
+    public static class MapExtensions
+    {
+        public static void PaintTiles(this Tilemap map, IEnumerable<Vector3Int> fillList, Tile fill)
+        {
+            foreach (Vector3Int location in fillList)
+                map.SetTile(location, fill);
+        }
+    }
+}
